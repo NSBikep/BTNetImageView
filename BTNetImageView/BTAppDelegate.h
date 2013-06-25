c02dc2618696835b795d3194a16d8b957c296b67
NSBikep <dlutwanghaoyu@ou-hiroshiumatoiMac.local>
2013-06-25 11:49:36 +0800

    Initial Commit

---
+++ BTNetImageView/BTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BTAppDelegate.h
+//  BTNetImageView
+//
+//  Created by 王 浩宇 on 13-6-25.
+//  Copyright (c) 2013年 王 浩宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BTNetImageView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BTNetImageView
+//
+//  Created by 王 浩宇 on 13-6-25.
+//  Copyright (c) 2013年 王 浩宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BTAppDelegate class]));
+    }
+}
